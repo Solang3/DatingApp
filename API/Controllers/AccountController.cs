4ae9b20da3fb1bcd0fbed22446ba5317ded643f1
Solange Gonzalez Ferrero <solangegf@gmail.com>
2024-05-26 14:18:28 -0300

    End of section 4

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,27 @@
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using API.Data;
+using API.interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors();
+            services.AddScoped<ITokenService, TokenService>();
+
+            return services;
+        }
+           
+    }
+}
