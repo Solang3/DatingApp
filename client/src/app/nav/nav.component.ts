f05fe12cabfccf5de4c773696882762f8260151f
Solange Gonzalez Ferrero <solangegf@gmail.com>
2024-05-29 20:00:49 -0300

    Error handling and routing

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service'
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -10,21 +12,24 @@ export class NavComponent implements OnInit {
 
   model: any = {}
   
-  constructor(public accountService: AccountService) {}
+  constructor(
+    public accountService: AccountService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
   }
   
   login() {
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
-      },
-      error: error => console.log(error)
+      next: _ => this.router.navigateByUrl('/members'),
+      error: error => this.toastr.error(error.error)
     })
   }
 
   logOut() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
